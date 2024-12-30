5b06aee9068a60ade3bf77049605179f834894a1
DharanyaSakthivel-SF4210 <Dharanya.sakthivel@syncfusion.com>
2024-12-30 21:10:37 +0530

    Task-895640-Blog-Convert-PowerPoint-to-Images

---
+++ convert-entire-powerpoint-to-images/.NET-Framework/convert-entire-powerpoint-to-images/Program.cs
@@ -0,0 +1,37 @@
+﻿using Syncfusion.Drawing;
+using Syncfusion.OfficeChartToImageConverter;
+using Syncfusion.Presentation;
+using System.IO;
+
+namespace convert_entire_powerpoint_to_images
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Open the file as Stream.
+            using (FileStream fileStream = new FileStream(Path.GetFullPath(@"../../Template.pptx"), FileMode.Open, FileAccess.Read))
+            {
+                //Load an existing PowerPoint presentation.
+                using (IPresentation pptxDoc = Presentation.Open(fileStream))
+                {
+                    //Initialize ChartToImageConverter
+                    pptxDoc.ChartToImageConverter = new ChartToImageConverter();
+                    //Convert PowerPoint to images.
+                    Stream[] images = pptxDoc.RenderAsImages((ImageFormat)ImageType.Metafile);
+                    //Save the images to file.
+                    for (int i = 0; i < images.Length; i++)
+                    {
+                        using (Stream stream = images[i])
+                        {
+                            using (FileStream fileStreamOutput = File.Create(Path.GetFullPath("../../Image-" + i + ".jpg")))
+                            {
+                                stream.CopyTo(fileStreamOutput);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ convert-entire-powerpoint-to-images/.NET-Framework/convert-entire-powerpoint-to-images/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("convert-entire-powerpoint-to-images")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("convert-entire-powerpoint-to-images")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1d26ac76-3d3e-4626-929e-d0936f49c5cb")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
